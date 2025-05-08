c1368a8458e891a5efdac8b692c8963890cbda01
ben.abbas <ben.abbas@opennet.tw>
2025-05-08 17:39:52 +0800

    Disable local deduplication

--- worker/trivy_proc.py
+++ worker/trivy_proc.py
@@ -118,11 +118,12 @@ def scan_and_upload_branch(repo_url, branch, repo_name, dojo_token, dojo_url, en
         return
 
     logger.info(f"Uploading findings file: {file_path}")
-    already_uploaded = hash_mgr.filter_new_trivy_findings("trivy", repo_name, branch, file_path)
+    #already_uploaded = hash_mgr.filter_new_trivy_findings("trivy", repo_name, branch, file_path)
+    #skip deduplication on local runs
+    already_uploaded = False
     logging.info(f"Already uploaded: {already_uploaded}")
 
     uploaded = False
-
     if not skip_dojo and not already_uploaded:
         chunks = split_trivy_findings(file_path, max_findings=50)
 
